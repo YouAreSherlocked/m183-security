c74fe341caba7abe1397bcd79d290f3ee3909cfc
Timo Mayer <mayer.timo@hotmail.com>
2021-01-18 12:14:28 +0100

    post new knight

--- ClientApp/src/components/Post.jsx
+++ ClientApp/src/components/Post.jsx
@@ -16,30 +16,32 @@ class Post extends Component {
 
     async submit(e) {
         e.preventDefault()
-        const image = e.target['image'].value
         const text = e.target['text'].value
-        await fetch(`${API_URL}/api/...`, {
+        const imageUrl = e.target['image'].value
+        await fetch(`${API_URL}/api/posts/create`, {
             method: 'POST',
             headers: {
+                'Authorization': localStorage.getItem('auth'),
                 'Content-Type': 'application/json',
                 'apikey': 'showdown'
             },
-            body: JSON.stringify({ 'image': image, 'text': text})
+            body: JSON.stringify({ 'text': text, 'imageUrl': imageUrl})
         }).then(res => res.text()).then(res => this.setState({output: res}))
+        alert('New Knight Created.')
     }
 
     render () {
     return (
         <Fragment>
-        <Header></Header>
-        <h2>Post new Knight</h2>
-        <form onSubmit={ e => this.submit(e) }>
-            <label htmlFor="image">Image (Url)</label>
-            <input type="text" name="image" id="image"></input>
-            <label htmlFor="text">Text</label>
-            <input type="text" name="text" id="text"></input>
-            <input type="submit" name="submit" id="submit" value="submit"></input>
-        </form>
+            <Header></Header>
+            <h2>Post new Knight</h2>
+            <form onSubmit={ e => this.submit(e) }>
+                <label htmlFor="text">Text</label>
+                <input type="text" name="text" id="text"></input>
+                <label htmlFor="image">Image (Url)</label>
+                <input type="text" name="image" id="image"></input>
+                <input type="submit" name="submit" id="submit" value="submit"></input>
+            </form>
         </Fragment>
     );
     }
